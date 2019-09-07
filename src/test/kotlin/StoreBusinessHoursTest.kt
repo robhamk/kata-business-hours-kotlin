b04f1981fa6db828fd04591085535f53b6ef953e
Robert Yen <robert.yen@linecorp.com>
2019-09-07 10:32:48 +0800

    project setup

---
+++ src/test/kotlin/StoreBusinessHoursTest.kt
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2019 LINE Corporation. All rights reserved.
+ * LINE Corporation PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+class StoreBusinessHoursTest {
+    
+}
