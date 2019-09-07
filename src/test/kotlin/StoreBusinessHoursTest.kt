945652072aa37710496259b08f802ba39c6a39f5
Robert Yen <robert.yen@linecorp.com>
2019-09-07 11:49:27 +0800

    Refactor test case

--- src/test/kotlin/StoreBusinessHoursTest.kt
+++ src/test/kotlin/StoreBusinessHoursTest.kt
@@ -1,7 +1,7 @@
 import junit.framework.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import java.time.DayOfWeek
-import java.time.Duration
 import java.time.LocalDateTime
 
 /*
@@ -10,25 +10,35 @@ import java.time.LocalDateTime
  */
 
 class StoreBusinessHoursTest {
+    private lateinit var storeBusinessHours: StoreBusinessHours
+
+    @Before
+    fun setup() {
+        storeBusinessHours = StoreBusinessHours()
+    }
+
     @Test
     fun testClose_MoreThanOneHoursBeforeOpen() {
-        var storeBusinessHours = StoreBusinessHours()
-        val beginHour = LiteralHour(DayOfWeek.MONDAY, 10, 0)
-        val endHour = LiteralHour(DayOfWeek.MONDAY, 12, 0)
-        var openingHours = OpeningHours(beginHour, endHour)
-        storeBusinessHours.setOpeningHours(listOf(openingHours))
+        givenOneBusinessHours()
 
-        assertEquals("Close", storeBusinessHours.query(LocalDateTime.of(2019, 9, 2, 8, 59)))
+        shouldBe("Close", LocalDateTime.of(2019, 9, 2, 8, 59))
     }
 
     @Test
     fun testOpen_OneHourBeforeOpen() {
-        var storeBusinessHours = StoreBusinessHours()
+        givenOneBusinessHours()
+
+        shouldBe("Open Soon", LocalDateTime.of(2019, 9, 2, 9, 0))
+    }
+
+    private fun givenOneBusinessHours() {
         val beginHour = LiteralHour(DayOfWeek.MONDAY, 10, 0)
         val endHour = LiteralHour(DayOfWeek.MONDAY, 12, 0)
-        var openingHours = OpeningHours(beginHour, endHour)
+        val openingHours = OpeningHours(beginHour, endHour)
         storeBusinessHours.setOpeningHours(listOf(openingHours))
+    }
 
-        assertEquals("Open Soon", storeBusinessHours.query(LocalDateTime.of(2019, 9, 2, 9, 0)))
+    private fun shouldBe(expectedResult: String, localDateTime: LocalDateTime) {
+        assertEquals(expectedResult, storeBusinessHours.query(localDateTime))
     }
 }
